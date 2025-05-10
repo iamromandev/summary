febd882ab1382b0e58953c6561d8ceac2c9e8e60
Roman <iamromandev@gmail.com>
2025-05-10 14:12:51 +0600

    run uvicorn on main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.scraper import get_raw_text
+from core.constants import WEB_URL
 
 app = FastAPI(title="Summary Application")
 
@@ -22,7 +24,13 @@ async def home():
     return {"message": "This is the home page"}
 
 
+@app.get("/scrape", tags=["scrape"])
+async def scrape():
+    text: str | None = await get_raw_text(WEB_URL)
+    return {"body": text or ""}
+
+
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
