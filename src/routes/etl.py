94150b64a8b2d515b4e146a1fc277a8319e86fe1
Roman <iamromandev@gmail.com>
2025-05-14 01:04:35 +0600

    run extract_links by background task

--- src/routes/etl.py
+++ src/routes/etl.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, BackgroundTasks, Depends
 from loguru import logger
 
 from src.core.constants import WEB_URL
@@ -12,8 +12,10 @@ router = APIRouter(prefix="/etl", tags=["etl"])
 
 @router.post("/")
 async def run_etl(
+    bt: BackgroundTasks,
     extract_service: Annotated[ExtractService, Depends(get_extract_service)]
 ):
     logger.info("Running ETL")
-    await extract_service.extract_urls(WEB_URL)
+    bt.add_task(extract_service.extract_urls, WEB_URL)
+    # await extract_service.extract_urls(WEB_URL)
     return None
