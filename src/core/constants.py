a081ceb35d3d05b4dfe06c5cb269b0555a8c4043
Roman <iamromandev@gmail.com>
2025-05-14 10:33:49 +0600

    extracted and saving erls in db

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,3 +1,4 @@
-# WEB_URL = "https://books.toscrape.com"
+#WEB_URL = "https://books.toscrape.com"
 WEB_URL = "https://www.thedailystar.net"
+#WEB_URL = "https://edition.cnn.com"
 PW_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
--- src/routes/etl.py
+++ src/routes/etl.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, BackgroundTasks, Depends
 from loguru import logger
 
 from src.core.constants import WEB_URL
+from src.db.models import Url
 from src.services.deps import get_extract_service
 from src.services.extract import ExtractService
 
@@ -12,10 +13,11 @@ router = APIRouter(prefix="/etl", tags=["etl"])
 
 @router.post("/")
 async def run_etl(
+   extract_service: Annotated[ExtractService, Depends(get_extract_service)],
     bt: BackgroundTasks,
-    extract_service: Annotated[ExtractService, Depends(get_extract_service)]
 ):
-    logger.info("Running ETL")
+    logger.info(f"Running ETL {WEB_URL}")
     bt.add_task(extract_service.extract_urls, WEB_URL)
-    # await extract_service.extract_urls(WEB_URL)
+    await Url.create(url=WEB_URL, base=WEB_URL)
+    #await extract_service.extract_urls(WEB_URL)
     return None
